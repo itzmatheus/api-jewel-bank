954a1837c3a06d86ef073630182127f9cd84e293
matheus.leite <matheusjoselfm@gmail.com>
2023-05-15 20:41:45 -0300

    test(AccountController): feat add api to create account from bank user id

---
+++ src/test/kotlin/com/jewelbank/api/controller/AccountControllerTest.kt
@@ -0,0 +1,57 @@
+package com.jewelbank.api.controller
+
+import com.jewelbank.api.entity.Account
+import com.jewelbank.api.entity.BankUser
+import com.jewelbank.api.repository.AccountRepository
+import com.jewelbank.api.repository.AgencyRepository
+import com.jewelbank.api.repository.BankRepository
+import com.jewelbank.api.utils.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+class AccountControllerTest {
+
+    @Autowired
+    lateinit var template: TestRestTemplate
+    @Autowired
+    lateinit var accountRepository: AccountRepository
+    @Autowired
+    lateinit var agencyRepository: AgencyRepository
+    @Autowired
+    lateinit var bankRepository: BankRepository
+
+    @BeforeEach
+    fun setUp() {
+        accountRepository.deleteAll()
+
+        agencyRepository.save(createAgencyMock())
+        bankRepository.save(createBankMock())
+    }
+
+    @Test
+    fun `Should create the account from a bank user id`() {
+
+        val requestRegisterUser = createRequestRegisterUser()
+        val responseCreateUser = template.postForEntity(ENDPOINT_BANK_USER_REGISTER, requestRegisterUser, BankUser::class.java)
+
+        val responseCreateAccount = template.postForEntity(
+            ENDPOINT_BANK_ACCOUNT_REGISTER.format(responseCreateUser.body?.id),
+            null,
+            Account::class.java,
+        )
+
+        Assertions.assertNotNull(responseCreateAccount.body?.id)
+        Assertions.assertEquals(createAgencyMock().name, responseCreateAccount.body?.agency?.name)
+        Assertions.assertEquals(createBankMock().name, responseCreateAccount.body?.bank?.name)
+        Assertions.assertEquals(responseCreateUser.body?.id, responseCreateAccount.body?.bankUser?.id)
+
+    }
+
+}
--- src/test/kotlin/com/jewelbank/api/utils/TemplateForTest.kt
+++ src/test/kotlin/com/jewelbank/api/utils/TemplateForTest.kt
@@ -2,6 +2,8 @@ package com.jewelbank.api.utils
 
 import com.jewelbank.api.dto.BankUserRegisterDTO
 import com.jewelbank.api.dto.LoginDTO
+import com.jewelbank.api.entity.Agency
+import com.jewelbank.api.entity.Bank
 
 const val NAME: String = "John Doe"
 const val PASSWORD: String = "password_123"
@@ -10,6 +12,7 @@ const val CPF: String = "00000000000"
 
 const val ENDPOINT_LOGIN: String = "/login"
 const val ENDPOINT_BANK_USER_REGISTER: String = "/bankuser/register"
+const val ENDPOINT_BANK_ACCOUNT_REGISTER: String = "/account/bankuser/%s/create"
 
 
 fun createRequestRegisterUser(
@@ -30,4 +33,20 @@ fun createRequestLoginDTO(
 ) = LoginDTO(
     email = email,
     password = password,
+)
+
+fun createAgencyMock(
+    name: String = "TEST AGENCY",
+    number: String = "123456"
+) = Agency(
+    name = name,
+    number = number
+)
+
+fun createBankMock(
+    name: String = "TEST BANK",
+    number: String = "123"
+) = Bank(
+    name = name,
+    number = number
 )
